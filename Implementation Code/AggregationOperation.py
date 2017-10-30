6b43d83bd5285beb3deccc54a4ac527e3b757ca0
Alex Nordin <anordin@mit.edu>
2017-10-30 10:31:11 -0400

    AggOp and TransOp Implementations
    Implemented simple forms of both the AggOp and TransOp classes

---
+++ Implementation Code/AggregationOperation.py
@@ -0,0 +1,39 @@
+from Operation import Operation
+import pandas as pd
+from SubOperation import SubOperation
+class AggregationOperation(Operation):
+	
+	#MULTIROW OPERATION TO BE APPLIED TO ALL COLUMNS FOR ALL ENTITIES
+	#AGGREGATES ALL ROWS INTO A SINGLE ROW
+
+	def last(dataframe):
+		return dataframe.tail(n = 1)
+	def first(dataframe):
+		return dataframe.head(n = 1)
+	possible_ag_opps = {
+		"LAST" : last,
+		"FIRST": first
+	}
+
+	"""
+	Args:
+		Takes as input a single aggregation operation to perform from the dictionary
+		of possible aggregation operations defined above. (TODO Find a good way 
+		to define the dictionaries location)
+	Returns:
+		None
+	Raises:
+	    None
+	"""
+	def __init__(self, multi_row_operation):
+		self.multi_row_operation = multi_row_operation
+
+	def execute(self, dataset):
+		return self.multi_row_operation(dataset)
+
+
+#SMALL TEST
+# df = pd.DataFrame([[74, 200, 22, "Alex"],[71, 140, 19, "Shea"], [75, 170, 20, "Abby"]], columns = ['height', 'weight', 'age', 'name'])
+# df.loc[3] = {"height":78, "name":"Future Alex", "weight":105, "age":25}
+# agg_op = AggregationOperation(AggregationOperation.possible_ag_opps["FIRST"])
+# print agg_op.execute(df)
---
+++ Implementation Code/TransformationOperation.py
@@ -0,0 +1,57 @@
+from Operation import Operation
+import pandas as pd
+from SubOperation import SubOperation
+class TransformationOperation(Operation):
+	
+	#MULTIROW OPERATION TO BE APPLIED TO ALL COLUMNS FOR ALL ENTITIES
+	#TRANFORMS ALL ROWS INTO A SINGLE ROW
+
+	def identity(dataframe):
+		return dataframe
+	
+
+	def diff(dataframe):
+		output_df = pd.DataFrame(columns = list(dataframe))
+		column_names = list(dataframe)
+		for idx, row in dataframe.iterrows():
+			if idx == 0:
+				continue
+			else:
+				new_row = {}
+				current_row = row
+				previous_row = dataframe.iloc[idx-1]
+				for column in column_names:
+					if type(current_row[column]) is not str:
+						diff = current_row[column] - previous_row[column]
+						print diff
+					else:
+						diff = current_row[column] + ' - ' + previous_row[column] #String Concat
+					new_row[column] = diff
+				output_df = output_df.append(new_row, ignore_index = True)
+		return output_df
+	possible_ag_opps = {
+		"IDENTITY" : identity,
+		"DIFF": diff
+	}
+
+	"""
+	Args:
+		
+	Returns:
+		None
+	Raises:
+	    None
+	"""
+	def __init__(self, transformation_operation):
+		self.transformation_operation = transformation_operation
+	
+	def execute(self, dataset):
+		return self.transformation_operation(dataset)
+
+#SMALL TEST
+# df = pd.DataFrame([[74, 200, 22, "Alex"],[71, 140, 19, "Shea"], [75, 170, 20, "Abby"]], columns = ['height', 'weight', 'age', 'name'])
+# df.loc[3] = {"height":78, "name":"Future Alex", "weight":105, "age":25}
+# trans_op = TransformationOperation(TransformationOperation.possible_ag_opps["DIFF"])
+# print df
+# print trans_op.execute(df)
+# print df

e97462daf246bad427e48c09697ede7b398a6894
Alex Nordin <anordin@mit.edu>
2017-11-07 11:33:00 -0500

    Built out spec and added label_generating_column

--- Implementation Code/PredictionProblemGenerator.py
+++ Implementation Code/PredictionProblemGenerator.py
@@ -14,19 +14,30 @@ class PredictionProblemGenerator:
 	#	TRANSFORMATION OPERATION : DIFF
 
 	
-
-
-	def __init__(self, dataframe):
+	"""
+	Args:
+		(Pandas Dataframe) dataframe: The dataset to be operated upon.
+		(String) label_generating_column: the column of interest. This column
+			will be solely used for performing operations against.
+	Returns:
+		None
+	"""
+	def __init__(self, dataframe, label_generating_column):
 		self.dataframe = dataframe
+		self.label_generating_column = label_generating_column
 
-	#Inspired by Ben Shreck's MIT MEng Thesis pg. 57, named LittleTrane.
-	#	LittleTrane imposes constraints on PredictionProblem definitions.
-	#	Beginning with a simple implementation.
-	#	FilterOp - RowOp - TransOp - AggOp
-	#We may expand the possibilities later, but for now we start simple.
-
+	"""
+	Inspired by Ben Shreck's MIT MEng Thesis pg. 57, named LittleTrane.
+		LittleTrane imposes constraints on PredictionProblem definitions.
+		Beginning with a simple implementation.
+		FilterOp - RowOp - TransOp - AggOp
+	We may expand the possibilities later, but for now we start simple.
+	Args:
+		None
+	Returns:
+		(List): A list of prediction problems.
+	"""	
 	#possible_operations for each class is a dictionary mapping a string name to an Operation of that class.
-
 	def generate(self):
 		possible_row_operation_names = RowOperation.possible_operations.keys()
 		possible_filter_operation_names = FilterOperation.possible_operations.keys()
@@ -34,8 +45,7 @@ class PredictionProblemGenerator:
 		possible_aggregation_operation_names = AggregationOperation.possible_operations.keys()
 		possible_transformation_operation_names = TransformationOperation.possible_operations.keys()
 
-		# column_names = list(self.dataframe)
-		column_to_operate_over = "height"
+		column_to_operate_over = self.label_generating_column
 		prediction_problems = []
 
 		for aggregation_operation_name in possible_aggregation_operation_names:
@@ -58,6 +68,6 @@ class PredictionProblemGenerator:
 
 df = pd.DataFrame([[74, 200, 22, "Alex"],[71, 140, 19, "Shea"], [75, 170, 20, "Abby"]], columns = ['height', 'weight', 'age', 'name'])
 df.loc[3] = {"height":78, "name":"Future Alex", "weight":105, "age":25}
-gen = PredictionProblemGenerator(df)
+gen = PredictionProblemGenerator(df, "height")
 pred_problems = gen.generate()
 print len([str(pred_problem) for pred_problem in pred_problems])

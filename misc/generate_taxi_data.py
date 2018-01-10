2d96ebbdea80d8bf873c640c29dae901ab7cf7a5
Alex Nordin <anordin@mit.edu>
2018-01-10 17:23:34 -0500

    Updated Old Taxi Data Generation Code
    Re-arranged folder layout of trade and the repo. Moved misc code that
    was in trane to the higher, repo-level Trane, thus preventing any
    non-essential parts being involved in the trane folder.

--- misc/generate_taxi_data.py
+++ misc/generate_taxi_data.py
@@ -1,27 +1,94 @@
 import pandas as pd
+import numpy as np
 import random
-columns = [
-    "taxi_id",
-    "trip_id",
-    "fare",
-    "num_passengers",
-    "trip_distance",
-    ]
+COLUMNS = [
+	"vendor_id",
+	"taxi_id",
+	"trip_id",
+	"distance",
+	"duration",
+	"fare",
+	"num_passengers",
+	]
 
-NUM_TAXIS = 3
-NUM_TRIPS_PER_TAXI = 10
-rows = []
-for taxi_num in range(NUM_TAXIS):
-    for trip_num in range(NUM_TRIPS_PER_TAXI):
-        for column in columns:
-            new_row = []
-            new_row.append(taxi_num)
-            new_row.append(trip_num)
-            fare = random.random()*10
-            new_row.append(fare)
-            new_row.append(random.randint(1,4))
-            distance = fare/2 + random.random()
-            new_row.append(distance)
-        rows.append(new_row)
-df = pd.DataFrame(rows, columns = columns)
+NUM_VENDORS = 5
+NUM_TAXIS_PER_VENDOR = 40
+NUM_TRIPS_PER_TAXI = 40
+
+def get_trip_distance():
+	mu = 5
+	sigma = 2
+	sample = np.random.normal(mu, sigma, 1)
+	if sample < 0:
+		sample = mu
+	sample = round(sample, 2)
+	return sample
+
+def get_trip_time():
+	mu = 15
+	sigma = 5
+	sample = np.random.normal(mu, sigma, 1)
+	if sample < 0:
+		sample = mu
+	sample = round(sample, 2)
+	return sample
+
+def get_trip_fare(trip_distance, trip_time):
+	#Based on DC TAXI fare schedule
+	return round(3 + 2.16 * trip_distance + 2 * trip_time, 2)
+
+def get_num_passengers():
+	return random.randint(1,4)
+
+
+def generate_row_of_data(vendor_id, taxi_id, trip_id):
+	row = []
+	row += [vendor_id, 
+			taxi_id, 
+			trip_id]
+
+	trip_distance = get_trip_distance()
+	trip_time = get_trip_time()
+	trip_fare = get_trip_fare(trip_distance, trip_time)
+	num_passengers = get_num_passengers()
+
+	row += [trip_distance, 
+			trip_time,
+			trip_fare,
+			num_passengers]
+	return row
+
+def generate_rows():
+	rows = []
+	for vendor_id in range(NUM_VENDORS):
+		for taxi_id in range(NUM_TAXIS_PER_VENDOR):
+			for trip_id in range(NUM_TRIPS_PER_TAXI):
+				row = generate_row_of_data(vendor_id, taxi_id, trip_id)
+				rows.append(row)
+	return rows
+
+def generate_data():
+	rows = generate_rows()
+	df = pd.DataFrame(rows, columns = COLUMNS)
+	return df
+	
+df = generate_data()
 df.to_csv("synthetic_taxi_data.csv", index = False)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

76f2a9406ba9132ed26d82ec7a6c6d537342a330
Alex Nordin <anordin@mit.edu>
2018-01-10 18:30:04 -0500

    Architecture Updates
    Removed label generator. Added a labeler that is responsible for
    transforming data from (entity_id, cutoff_time, data) to (entity_id,
    cutoff_time, label). Combined Csv2Df and DataParser. Created the
    framework for Generating Cutoff Times.

--- trane/utils/data_parser.py
+++ trane/utils/data_parser.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from functools import reduce
 
 def df_to_entity_id_to_df(dataframe, entity_id_column_name):
     """
@@ -16,6 +18,33 @@ def df_to_entity_id_to_df(dataframe, entity_id_column_name):
         relevant_data = dataframe.loc[dataframe[entity_id_column_name] == entity_id]
         entity_id_to_df[entity_id] = relevant_data
     return entity_id_to_df
+
+def csv_to_df(csv_file_paths, header = True):
+    """
+    Args:
+        (List)csv_file_paths: A list of the paths to the csv files you want to load
+        (Boolean)header: are there headers to the data columns?
+    Returns:
+        (List[dataframe])dataframes: A list of dataframes converted from the csv's, in the same order they were input
+    """
+    if not header:
+        dataframes = [pd.read_csv(file_path, header = None) for file_path in csv_file_paths]
+    else:
+       dataframes = [pd.read_csv(file_path) for file_path in csv_file_paths]
+
+    return dataframes
+
+def denormalize_dataframes(dataframes):
+    """
+    Args:
+        (List)dataframes: A list of the dataframes to combine and denormalize
+    Returns:
+        Dataframe: a single de-normalized dataframe structured from all the input dataframes.
+    """
+    merged_df = reduce((lambda left_frame, right_frame: pd.merge(left_frame, right_frame, how = 'outer')), dataframes)
+    merged_df.to_csv('merged_df.csv')
+    return merged_df
+
 
 if __name__ == "__main__":
     df = pd.read_csv('../../test_datasets/synthetic_taxi_data.csv')

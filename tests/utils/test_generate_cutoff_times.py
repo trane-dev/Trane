7761c08f44773061654a1cd337dfe28618370915
Lei Xu <leix@mit.edu>
2018-01-11 12:09:03 -0500

    unit tests for utils.

---
+++ tests/utils/test_generate_cutoff_times.py
@@ -0,0 +1,14 @@
+from trane.utils.generate_cutoff_times import *
+from pandas import DataFrame
+
+
+def test_fixed_cutoff_time():
+    cutoff_strategy = FixedCutoffTimes()
+    data = {
+        'entity1': DataFrame({'col1': [1, 2, 3]}),
+        'entity2': DataFrame({'col1': [4, 5, 6]})
+    }
+    cutoffs = cutoff_strategy.generate_cutoffs(data)
+    assert type(cutoffs) == dict
+    assert cutoffs['entity1'][1] == 0
+    assert cutoffs['entity2'][1] == 0

d97a8dffe91f69eb73246618f9df9a5bc499433a
Alex Nordin <anordin@mit.edu>
2018-01-11 17:10:10 -0500

    Architecture Updates
    Combine the functions csv_to_df and denormalize_dataframes. Change the
    structure of generate_cutoff_times to be more modular.

--- trane/utils/data_parser.py
+++ trane/utils/data_parser.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from functools import reduce
 
-def df_to_entity_id_to_df(dataframe, entity_id_column_name):
+def df_group_by_entity_id(dataframe, entity_id_column_name):
     """
     Convert a dataframe with an entity_id column to a dictionary mapping entity id's to their relevant data.
     Args:
@@ -25,30 +25,13 @@ def csv_to_df(csv_file_paths, header = True):
         (List)csv_file_paths: A list of the paths to the csv files you want to load
         (Boolean)header: are there headers to the data columns?
     Returns:
-        (List[dataframe])dataframes: A list of dataframes converted from the csv's, in the same order they were input
+        Dataframe: a single de-normalized dataframe structured from all the input csv's.
     """
     if not header:
         dataframes = [pd.read_csv(file_path, header = None) for file_path in csv_file_paths]
     else:
        dataframes = [pd.read_csv(file_path) for file_path in csv_file_paths]
 
-    return dataframes
-
-def denormalize_dataframes(dataframes):
-    """
-    Args:
-        (List)dataframes: A list of the dataframes to combine and denormalize
-    Returns:
-        Dataframe: a single de-normalized dataframe structured from all the input dataframes.
-    """
     merged_df = reduce((lambda left_frame, right_frame: pd.merge(left_frame, right_frame, how = 'outer')), dataframes)
     merged_df.to_csv('merged_df.csv')
     return merged_df
-
-
-if __name__ == "__main__":
-    df = pd.read_csv('../../test_datasets/synthetic_taxi_data.csv')
-    print(df)
-    print(df_to_entity_id_to_df(df, 'taxi_id')[0])
-    print(df_to_entity_id_to_df(df, 'taxi_id')[1])
-    print(df_to_entity_id_to_df(df, 'taxi_id')[2])

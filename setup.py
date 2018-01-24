356f14549c58727d92e755ae638beefc57a9be89
Lei Xu <leix@mit.edu>
2018-01-24 16:23:19 -0500

    fix pip install

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='trane',
       version='0.0',
       description='Trane is a software package for automatically generating prediction problems and generating labels for supervised learning.',
       url='https://github.com/HDI-Project/Trane',
+      packages=find_packages(),
       author='DAI',
       author_email='',
       license='MIT')

7048fe9661e390176ffe36b82a9dbe7a3aeac2e8
Alex Nordin <anordin@mit.edu>
2018-04-12 12:42:05 -0400

    new setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
 from setuptools import setup, find_packages
 
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'pandas>=0.21.0',
+    'numpy>=1.13.0',
+    'python_dateutil>=2.6.0',
+    'scipy>=1.0.0'
+    ]
+
+setup_requirements = ['pytest-runner', ]
 
 test_requirements = ['pytest', ]
 
-
-setup(name='trane',
-      version='0.0',
-      description='Trane is a software package for automatically generating prediction problems and generating labels for supervised learning.',
-      url='https://github.com/HDI-Project/Trane',
-      packages=find_packages(),
-      license='MIT License',
-      include_package_data = True,
-      author='MIT Data to AI Lab',
-      author_email = 'dailabmit@gmail.com',
-      tests_require = test_requirements,
-      classifiers=[
+setup(
+    author="MIT Data to AI Lab",
+    author_email='dai-lab-trane@mit.edu',
+    classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -23,6 +34,20 @@ setup(name='trane',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-    	]
-
-      )
+    ],
+    description="Trane is a software package for automatically generating prediction problems and generating labels for supervised learning.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    keywords='Trane',
+    name='Trane',
+    packages=find_packages(include=['Trane', 'Trane.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/HDI-Project/Trane',
+    version='0.1.0',
+    zip_safe=False,
+)

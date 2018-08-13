887a3b575e7db2d4577312dd1529b0168e4659e4
Albert Carter <albert.r.carter@gmail.com>
2018-08-13 14:22:24 +0200

    set up test framework for prediction_problem_generator

--- tests/core/test_prediction_problem_generator.py
+++ tests/core/test_prediction_problem_generator.py
@@ -2,27 +2,83 @@
 Function: generate(self)
 1. Ensure the correct # of problems are generated.
 2. Generated problems are of proper type
-3. Ensure prediction problems are generated in order of Filter->Row->Transformation->Aggregation
+3. Ensure prediction problems are generated in order of
+   Filter->Row->Transformation->Aggregation
 """
+import unittest
 
 import pandas as pd
+from mock import MagicMock, patch
 
-from trane.core.prediction_problem import *  # noqa
-from trane.core.prediction_problem_generator import *  # noqa
+from trane.core.prediction_problem_generator import PredictionProblemGenerator
 from trane.ops.aggregation_ops import *  # noqa
 from trane.ops.filter_ops import *  # noqa
 from trane.ops.row_ops import *  # noqa
 from trane.ops.transformation_ops import *  # noqa
+from trane.utils.table_meta import TableMeta
 
-meta_json_str = '{ "path": "", "tables": [ { "path": "synthetic_taxi_data.csv",\
-    "name": "taxi_data", "fields": [ {"name": "vendor_id", "type": "id"},\
-    {"name": "taxi_id", "type": "id"}, {"name": "trip_id", "type": "datetime"}, \
-    {"name": "distance", "type": "number", "subtype": "float"}, \
-    {"name": "duration", "type": "number", "subtype": "float"}, \
-    {"name": "fare", "type": "number", "subtype": "float"}, \
-    {"name": "num_passengers", "type": "number", "subtype": "float"} ] } ]}'
-dataframe = pd.DataFrame([(0, 0, 0, 5.32, 19.7, 53.89, 1),
-                          (0, 0, 1, 1.08, 6.78, 18.89, 2),
-                          (0, 0, 2, 4.69, 14.11, 41.35, 4)],
-                         columns=["vendor_id", "taxi_id", "trip_id", "distance",
-                                  "duration", "fare", "num_passengers"])
+
+class TestPredictionProblemGenerator(unittest.TestCase):
+
+    def setUp(self):
+        self.table_meta_mock = MagicMock()
+        self.entity_id_col = "taxi_id"
+        self.label_generating_col = "fare"
+        self.time_col = "trip_id"
+        self.filter_col = "taxi_id"
+
+        self.ensure_valid_inputs_patch = self.create_patch(
+            'trane.core.PredictionProblemGenerator.ensure_valid_inputs')
+
+        self.generator = PredictionProblemGenerator(
+            table_meta=self.table_meta_mock,
+            entity_id_col=self.entity_id_col,
+            label_generating_col=self.label_generating_col,
+            time_col=self.time_col,
+            filter_col=self.filter_col)
+
+    def prep_for_integration(self):
+        '''
+        Creates a full fledged prediction problem generator without
+        a mocked out ensure_valid_inputs method
+        '''
+        meta_json_str = ' \
+            {"path": "", \
+             "tables": [ \
+                {"path": "synthetic_taxi_data.csv",\
+                 "name": "taxi_data", \
+                 "fields": [ \
+                {"name": "vendor_id", "type": "id"},\
+                {"name": "taxi_id", "type": "id"}, \
+                {"name": "trip_id", "type": "datetime"}, \
+                {"name": "distance", "type": "number", "subtype": "float"}, \
+                {"name": "duration", "type": "number", "subtype": "float"}, \
+                {"name": "fare", "type": "number", "subtype": "float"}, \
+                {"name": "num_passengers", "type": "number", \
+                    "subtype": "float"} \
+                 ]}]}'
+        self.table_meta = TableMeta.from_json(meta_json_str)
+        self.dataframe = pd.DataFrame(
+            [(0, 0, 0, 5.32, 19.7, 53.89, 1),
+             (0, 0, 1, 1.08, 6.78, 18.89, 2),
+             (0, 0, 2, 4.69, 14.11, 41.35, 4)],
+            columns=["vendor_id", "taxi_id", "trip_id", "distance", "duration",
+                     "fare", "num_passengers"])
+
+        self.generator = PredictionProblemGenerator(
+            table_meta=self.table_meta,
+            entity_id_col=self.entity_id_col,
+            label_generating_col=self.label_generating_col,
+            time_col=self.time_col,
+            filter_col=self.filter_col)
+
+    def create_patch(self, name, return_value=None):
+        '''helper method for creating patches'''
+        patcher = patch(name)
+        thing = patcher.start()
+        self.addCleanup(patcher.stop)
+
+        if return_value:
+            thing.return_value = return_value
+
+        return thing
--- trane/core/prediction_problem_generator.py
+++ trane/core/prediction_problem_generator.py
@@ -12,20 +12,20 @@ class PredictionProblemGenerator:
     Object for generating prediction problems on data.
     """
 
-    def __init__(self, table_meta, entity_id_column,
-                 label_generating_column, time_column, filter_column):
+    def __init__(self, table_meta, entity_id_col,
+                 label_generating_col, time_col, filter_col):
         """
         Parameters
         ----------
         table_meta: TableMeta object. Contains
             meta information about the data
-        entity_id_column: column name of
+        entity_id_col: column name of
             the column containing entities in the data
-        label_generating_column: column name of the
+        label_generating_col: column name of the
             column of interest in the data
-        time_column: column name of the column
+        time_col: column name of the column
             containing time information in the data
-        filter_column: column name of the column
+        filter_col: column name of the column
             to be filtered over
 
         Returns
@@ -33,10 +33,10 @@ class PredictionProblemGenerator:
         None
         """
         self.table_meta = table_meta
-        self.entity_id_column = entity_id_column
-        self.label_generating_column = label_generating_column
-        self.time_column = time_column
-        self.filter_column = filter_column
+        self.entity_id_col = entity_id_col
+        self.label_generating_col = label_generating_col
+        self.time_col = time_col
+        self.filter_col = filter_col
         self.ensure_valid_inputs()
 
     def generate(self, dataframe):
@@ -52,6 +52,8 @@ class PredictionProblemGenerator:
         -------
         problems: a list of Prediction Problem objects.
         """
+
+
         def iter_over_ops():
             for aggregation_op_name in aggregation_ops.AGGREGATION_OPS:
                 for transformation_op_name in transformation_ops.TRANSFORMATION_OPS:
@@ -62,18 +64,18 @@ class PredictionProblemGenerator:
 
         problems = []
         for ops in iter_over_ops():
-            # for filter_column in self.table_meta.get_columns():
+            # for filter_col in self.table_meta.get_columns():
             aggregation_op_name, transformation_op_name, \
                 row_op_name, filter_op_name = ops
 
             aggregation_op_obj = getattr(aggregation_ops, aggregation_op_name)(
-                self.label_generating_column)
+                self.label_generating_col)
             transformation_op_obj = getattr(transformation_ops, transformation_op_name)(
-                self.label_generating_column)
+                self.label_generating_col)
             row_op_obj = getattr(row_ops, row_op_name)(
-                self.label_generating_column)
+                self.label_generating_col)
             filter_op_obj = getattr(
-                filter_ops, filter_op_name)(self.filter_column)
+                filter_ops, filter_op_name)(self.filter_col)
 
             operations = [
                 filter_op_obj,
@@ -85,11 +87,11 @@ class PredictionProblemGenerator:
 
             logging.debug("prediction problem generated, now checking validity...")
 
-            (is_valid_prediction_problem, filter_column_order_of_types,
-                label_generating_column_order_of_types) = \
+            (is_valid_prediction_problem, filter_col_order_of_types,
+                label_generating_col_order_of_types) = \
                 prediction_problem.is_valid_prediction_problem(
-                self.table_meta, self.filter_column,
-                self.label_generating_column)
+                self.table_meta, self.filter_col,
+                self.label_generating_col)
             if not is_valid_prediction_problem:
                 logging.debug("invalid prediction problem")
                 continue
@@ -97,9 +99,9 @@ class PredictionProblemGenerator:
             logging.debug("valid prediction problem, now generating and setting hyper parameters...")
 
             prediction_problem.generate_and_set_hyper_parameters(dataframe,
-                                                                 self.label_generating_column,
-                                                                 self.filter_column,
-                                                                 self.entity_id_column)
+                                                                 self.label_generating_col,
+                                                                 self.filter_col,
+                                                                 self.entity_id_col)
 
             logging.debug(
                 "valid problem with parameters generated: {} \n".format(prediction_problem))
@@ -108,11 +110,15 @@ class PredictionProblemGenerator:
         return problems
 
     def ensure_valid_inputs(self):
-        assert(self.table_meta.get_type(self.entity_id_column)
+        """
+        TypeChecking for the problem generator entity_id_col
+        and label_generating_col. Errors if types don't match up.
+        """
+        assert(self.table_meta.get_type(self.entity_id_col)
                in [TableMeta.TYPE_IDENTIFIER, TableMeta.TYPE_TEXT,
                    TableMeta.TYPE_CATEGORY])
-        assert(self.table_meta.get_type(self.label_generating_column)
+        assert(self.table_meta.get_type(self.label_generating_col)
                in [TableMeta.TYPE_FLOAT, TableMeta.TYPE_INTEGER,
                    TableMeta.TYPE_TEXT])
-        assert(self.table_meta.get_type(self.time_column)
+        assert(self.table_meta.get_type(self.time_col)
                in [TableMeta.TYPE_TIME, TableMeta.TYPE_INTEGER])

a84b6f53f4691540f4d888546079faaecdb31d85
Albert Carter <albert.r.carter@gmail.com>
2018-08-29 13:10:20 +0200

    remove labeler/conductor. Abandon multi-threading plans

--- trane/core/__init__.py
+++ trane/core/__init__.py
@@ -1,4 +1,3 @@
 from .prediction_problem_generator import PredictionProblemGenerator # noqa
 from .prediction_problem import PredictionProblem # noqa
 from .cutoff_strategy import CutoffStrategy # noqa
-from .conductor import Conductor  # noqa

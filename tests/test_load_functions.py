4da8ed11e9bd517a3cc4bc842a7f8420b17bc208
Patrik Đurđević <patrik.durdevic@gmail.com>
2023-07-31 10:13:09 -0400

    Add load_instacart test

--- tests/test_load_functions.py
+++ tests/test_load_functions.py
@@ -1,6 +1,7 @@
 from trane.datasets.load_functions import (
     load_covid,
     load_covid_metadata,
+    load_instacart,
     load_youtube,
     load_youtube_metadata,
 )
@@ -46,8 +47,36 @@ def test_load_youtube():
     assert metadata["trending_date"] == ColumnSchema(logical_type=Datetime)
 
 
+def test_load_instacart():
+    dataframes, relationships = load_instacart()
+    expected_columns = {
+        "orders": [
+            "order_id",
+            "user_id",
+            "eval_set",
+            "order_number",
+            "order_dow",
+            "order_hour_of_day",
+            "days_since_prior_order",
+            "order_date",
+        ],
+        "order_products": ["order_id", "product_id", "add_to_cart_order", "reordered"],
+        "products": ["product_id", "product_name", "aisle_id", "department_id"],
+        "aisles": ["aisle_id", "aisle"],
+        "departments": ["department_id", "department"],
+    }
+    check_columns(dataframes, expected_columns)
+    assert dataframes["orders"][0]["order_date"].dtype == "datetime64[ns]"
+
+
 def check_column_schema(columns, df, metadata):
     for col in columns:
         assert col in df.columns
         assert col in metadata.keys()
         assert isinstance(metadata[col], ColumnSchema)
+
+
+def check_columns(dataframes, columns):
+    for key, dataframe in dataframes.items():
+        dataframe = dataframe[0]
+        assert sorted(dataframe.columns) == sorted(columns[key])
--- trane/datasets/load_functions.py
+++ trane/datasets/load_functions.py
@@ -42,26 +42,49 @@ def load_youtube():
     df = df.fillna(0)
     return df
 
+
 def load_instacart():
     dataframes = {
-        "orders": (pd.read_csv(generate_local_filepath("data/instacart/orders.csv")), "id"),
-        "order_products": (pd.read_csv(generate_local_filepath("data/instacart/order_products.csv")), "id"),
-        "products": (pd.read_csv(generate_local_filepath("data/instacart/products.csv")), "id"),
-        "aisles": (pd.read_csv(generate_local_filepath("data/instacart/aisles.csv")), "id"),
-        "departments": (pd.read_csv(generate_local_filepath("data/instacart/departments.csv")), "id"),
+        "orders": (
+            pd.read_csv(generate_local_filepath("data/instacart/orders.csv")),
+            "id",
+        ),
+        "order_products": (
+            pd.read_csv(generate_local_filepath("data/instacart/order_products.csv")),
+            "id",
+        ),
+        "products": (
+            pd.read_csv(generate_local_filepath("data/instacart/products.csv")),
+            "id",
+        ),
+        "aisles": (
+            pd.read_csv(generate_local_filepath("data/instacart/aisles.csv")),
+            "id",
+        ),
+        "departments": (
+            pd.read_csv(generate_local_filepath("data/instacart/departments.csv")),
+            "id",
+        ),
     }
-    dataframes["orders"][0]["order_date"] = pd.to_datetime("2023-01-01") + pd.to_timedelta(dataframes["orders"][0]["order_id"] // 10000, unit="D")
-    dataframes["orders"][0]["order_date"] = pd.to_datetime(dataframes["orders"][0]["order_date"])
-    dataframes["orders"][0]["user_id"] = dataframes["orders"][0]["user_id"].astype("int")
-    
+    dataframes["orders"][0]["order_date"] = pd.to_datetime(
+        "2023-01-01"
+    ) + pd.to_timedelta(dataframes["orders"][0]["order_id"] // 10000, unit="D")
+    dataframes["orders"][0]["order_date"] = pd.to_datetime(
+        dataframes["orders"][0]["order_date"]
+    )
+    dataframes["orders"][0]["user_id"] = dataframes["orders"][0]["user_id"].astype(
+        "int"
+    )
+
     relationships = [
         ("aisles", "aisle_id", "products", "aisle_id"),
         ("departments", "department_id", "products", "department_id"),
         ("products", "product_id", "order_products", "product_id"),
         ("orders", "order_id", "order_products", "order_id"),
     ]
-    
+
     return dataframes, relationships
+
 
 def load_covid_metadata():
     table_meta = {

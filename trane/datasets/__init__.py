8a8d7e30e4fdffb313808a8f6450d5b61914d436
Patrik Đurđević <patrik.durdevic@gmail.com>
2023-07-31 10:02:52 -0400

    Update datasets and add Instacart

--- trane/datasets/__init__.py
+++ trane/datasets/__init__.py
@@ -3,4 +3,5 @@ from trane.datasets.load_functions import (
     load_covid_metadata,
     load_youtube,
     load_youtube_metadata,
+    load_instacart,
 )
--- trane/datasets/load_functions.py
+++ trane/datasets/load_functions.py
@@ -12,7 +12,7 @@ from trane.typing.logical_types import (
 
 
 def load_covid():
-    filepath = generate_local_filepath("covid19.csv")
+    filepath = generate_local_filepath("data/covid/covid19.csv")
     df = pd.read_csv(filepath)
     df["Date"] = pd.to_datetime(df["Date"], format="%m/%d/%y")
     df = df[
@@ -35,13 +35,33 @@ def load_covid():
 
 def load_youtube():
     time_col = "trending_date"
-    filepath = generate_local_filepath("USvideos.csv")
+    filepath = generate_local_filepath("data/youtube/USvideos.csv")
     df = pd.read_csv(filepath)
     df[time_col] = pd.to_datetime(df[time_col], format="%y.%d.%m")
     df = df.sort_values(by=[time_col])
     df = df.fillna(0)
     return df
 
+def load_instacart():
+    dataframes = {
+        "orders": (pd.read_csv(generate_local_filepath("data/instacart/orders.csv")), "id"),
+        "order_products": (pd.read_csv(generate_local_filepath("data/instacart/order_products.csv")), "id"),
+        "products": (pd.read_csv(generate_local_filepath("data/instacart/products.csv")), "id"),
+        "aisles": (pd.read_csv(generate_local_filepath("data/instacart/aisles.csv")), "id"),
+        "departments": (pd.read_csv(generate_local_filepath("data/instacart/departments.csv")), "id"),
+    }
+    dataframes["orders"][0]["order_date"] = pd.to_datetime("2023-01-01") + pd.to_timedelta(dataframes["orders"][0]["order_id"] // 10000, unit="D")
+    dataframes["orders"][0]["order_date"] = pd.to_datetime(dataframes["orders"][0]["order_date"])
+    dataframes["orders"][0]["user_id"] = dataframes["orders"][0]["user_id"].astype("int")
+    
+    relationships = [
+        ("aisles", "aisle_id", "products", "aisle_id"),
+        ("departments", "department_id", "products", "department_id"),
+        ("products", "product_id", "order_products", "product_id"),
+        ("orders", "order_id", "order_products", "order_id"),
+    ]
+    
+    return dataframes, relationships
 
 def load_covid_metadata():
     table_meta = {

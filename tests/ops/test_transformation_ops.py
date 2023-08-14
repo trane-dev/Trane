8e79f44fd0281d2b0a515e92de5d5548fa9024bc
Patrik Đurđević <patrik.durdevic@gmail.com>
2023-08-14 18:21:34 -0400

    Add Order By Operation (#138)
    * Add order by WIP
    
    * WIP
    
    * WIP
    
    * Lint
    
    * Update test_utils.py
    
    * Lint
    
    * Add IdentityOp
    
    * Update changelog.md
    
    * Update transformation_ops.py
    
    * Update transformation_ops.py
    
    * Update transformation_ops.py
    
    * Update transformation_ops.py
    
    * Update test
    
    * Add transformation tests
    
    * Lint

---
+++ tests/ops/test_transformation_ops.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import pytest
+
+from trane.ops.transformation_ops import IdentityOp, OrderByOp
+
+
+@pytest.fixture
+def df():
+    df = pd.DataFrame({"col": [5, 3, 4, 1, 2]})
+    return df
+
+
+def test_identity_transformation_op(df):
+    op = IdentityOp("col")
+    output = op(df)
+    assert output["col"].tolist() == df["col"].tolist()
+    assert op.generate_description() == ""
+
+
+def test_order_by_transformation_op_empty(df):
+    op = OrderByOp("col")
+    output = op(df)
+    assert (output.values == df.sort_values(by="col").values).all()
+    assert "sorted by <col>" in op.generate_description()
--- trane/ops/utils.py
+++ trane/ops/utils.py
@@ -1,5 +1,6 @@
 from trane.ops.aggregation_ops import AggregationOpBase
 from trane.ops.filter_ops import FilterOpBase
+from trane.ops.transformation_ops import TransformationOpBase
 
 
 def get_aggregation_ops():
@@ -8,3 +9,7 @@ def get_aggregation_ops():
 
 def get_filter_ops():
     return FilterOpBase.__subclasses__()
+
+
+def get_transformation_ops():
+    return TransformationOpBase.__subclasses__()

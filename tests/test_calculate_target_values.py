d5cd027d76fdf79836f647771b23ff15f2751417
Gaurav Sheni <gvsheni@gmail.com>
2023-10-02 14:21:28 -0400

    updates

--- tests/test_calculate_target_values.py
+++ tests/test_calculate_target_values.py
@@ -1,8 +1,10 @@
+from datetime import datetime
+
 import numpy as np
 import pandas as pd
 import pytest
 
-from trane.core.utils import calculate_target_values
+from trane.core.utils import calculate_target_values, clean_date, cutoff_data, to_offset
 from trane.metadata import SingleTableMetadata
 from trane.utils.testing_utils import create_mock_data, create_mock_data_metadata
 
@@ -10,6 +12,16 @@ from trane.utils.testing_utils import create_mock_data, create_mock_data_metadat
 @pytest.fixture()
 def data():
     df = create_mock_data(return_single_dataframe=True)
+    return df
+
+
+@pytest.fixture()
+def cutoff_df():
+    df = pd.DataFrame(
+        {"A": [1, 2, 3, 4, 5]},
+        index=pd.date_range("2022-01-01", periods=5, freq="D"),
+    )
+    df.index = pd.to_datetime(df.index)
     return df
 
 
@@ -56,3 +68,101 @@ def test_create_target_values(data):
         target_values["sum_amount"].tolist(),
         [13837.71, 12990.74, 11213.85],
     )
+
+
+def test_to_offset_int():
+    assert to_offset(5) == 5
+
+
+def test_to_offset_str():
+    assert to_offset("2d") == pd.Timedelta(2, unit="D")
+
+
+def test_to_offset_timedelta():
+    assert to_offset(pd.Timedelta(3, unit="H")) == pd.Timedelta(3, unit="H")
+
+
+def test_to_offset_offset():
+    assert to_offset(
+        pd.tseries.offsets.BusinessDay(2),
+    ) == pd.tseries.offsets.BusinessDay(2)
+
+
+def test_to_offset_invalid():
+    with pytest.raises(AssertionError):
+        to_offset(-1)
+    with pytest.raises(AssertionError):
+        to_offset("0d")
+    with pytest.raises(AssertionError):
+        to_offset(pd.Timedelta(-1, unit="H"))
+    with pytest.raises(AssertionError):
+        to_offset(pd.tseries.offsets.BusinessDay(-1))
+
+
+def test_clean_date():
+    assert clean_date("2019-01-01") == pd.Timestamp(
+        datetime.strptime("2019-01-01", "%Y-%m-%d"),
+    )
+    timestamp = pd.Timestamp(datetime.strptime("2019-01-01", "%Y-%m-%d"))
+    assert clean_date(timestamp) == timestamp
+
+
+def test_cutoff_data_int(cutoff_df):
+    threshold = 2
+    df_out, cutoff_time_out = cutoff_data(cutoff_df, threshold=threshold)
+
+    expected_df = pd.DataFrame(
+        {"A": [3, 4, 5]},
+        index=pd.date_range("2022-01-03", periods=3, freq="D"),
+    )
+    expected_df.index = pd.to_datetime(expected_df.index)
+    expected_cutoff_time = pd.Timestamp("2022-01-03")
+    pd.testing.assert_frame_equal(df_out, expected_df, check_freq=False)
+    assert cutoff_time_out == expected_cutoff_time
+
+
+def test_cutoff_data_str_offset(cutoff_df):
+    threshold = "2D"
+    df_out, cutoff_time_out = cutoff_data(cutoff_df, threshold=threshold)
+
+    expected_df = pd.DataFrame(
+        {"A": [3, 4, 5]},
+        index=pd.date_range("2022-01-03", periods=3, freq="D"),
+    )
+    expected_cutoff_time = pd.Timestamp("2022-01-03")
+    pd.testing.assert_frame_equal(df_out, expected_df, check_freq=False)
+    assert cutoff_time_out == expected_cutoff_time
+
+
+@pytest.mark.parametrize("threshold", ["2022-01-03", pd.Timestamp("2022-01-03")])
+def test_cutoff_data_str_timestamp(cutoff_df, threshold):
+    df_out, cutoff_time_out = cutoff_data(cutoff_df, threshold=threshold)
+    expected_df = pd.DataFrame(
+        {"A": [3, 4, 5]},
+        index=pd.date_range("2022-01-03", periods=3, freq="D"),
+    )
+    expected_cutoff_time = pd.Timestamp("2022-01-03")
+    pd.testing.assert_frame_equal(df_out, expected_df, check_freq=False)
+    assert cutoff_time_out == expected_cutoff_time
+
+
+def test_cutoff_invalid_threshold(cutoff_df):
+    threshold = "invalid"
+    with pytest.raises(ValueError):
+        cutoff_data(cutoff_df, threshold)
+
+
+def test_empty_data():
+    df = pd.DataFrame(
+        {"A": []},
+        index=pd.DatetimeIndex([], dtype="datetime64[ns]", freq=None),
+    )
+    threshold = 2
+    expected_df = pd.DataFrame(
+        {"A": []},
+        index=pd.DatetimeIndex([], dtype="datetime64[ns]", freq=None),
+    )
+    expected_cutoff_time = None
+    df_out, cutoff_time_out = cutoff_data(df, threshold)
+    pd.testing.assert_frame_equal(df_out, expected_df)
+    assert cutoff_time_out == expected_cutoff_time
--- trane/core/utils.py
+++ trane/core/utils.py
@@ -1,24 +1,61 @@
 from datetime import datetime
 
 import pandas as pd
+from pandas.tseries.offsets import DateOffset
 
 
-def clean_date(date):
-    if isinstance(date, str):
-        return pd.Timestamp(datetime.strptime(date, "%Y-%m-%d"))
-    return date
+def calculate_target_values(
+    df,
+    target_dataframe_index,
+    labeling_function,
+    time_index,
+    window_size,
+    minimum_data=None,
+    maximum_data=None,
+    gap=None,
+    drop_empty=True,
+    verbose=False,
+    num_examples_per_instance=-1,
+):
+    assert labeling_function, "missing labeling function(s)"
+
+    if df.index.name != time_index:
+        df = df.set_index(time_index)
+
+    if isinstance(df[target_dataframe_index].dtype, pd.CategoricalDtype):
+        df[target_dataframe_index] = df[
+            target_dataframe_index
+        ].cat.remove_unused_categories()
+
+    records = []
+    label_name = _get_function_name(labeling_function)
+    for group_key, df_by_index in df.groupby(target_dataframe_index):
+        for dataslice, metadata in generate_data_slices(
+            df_by_index,
+            window_size=window_size,
+            gap=gap,
+            min_data=minimum_data,
+            drop_empty=drop_empty,
+        ):
+            record = labeling_function(dataslice)
+            records.append(
+                {
+                    target_dataframe_index: group_key,
+                    "cutoff_time": dataslice.first_valid_index(),
+                    label_name: record,
+                },
+            )
+    records = pd.DataFrame.from_records(records, index=None)
+    return records
 
 
-def _bar_format(target_dataframe_index):
-    """Template to format the progress bar during a label search."""
-    value = "Elapsed: {elapsed} | "
-    value += "Remaining: {remaining} | "
-    value += "Progress: {l_bar}{bar}| "
-    value += target_dataframe_index + ": {n}/{total} "
-    return value
-
-
-def generate_data_slices(df, window_size, gap=None, min_data=None, drop_empty=True):
+def generate_data_slices(
+    df,
+    window_size=None,
+    gap=None,
+    min_data=None,
+    drop_empty=True,
+):
     window_size = window_size or len(df)
     gap = to_offset(gap or window_size)
 
@@ -94,95 +131,40 @@ def generate_data_slices(df, window_size, gap=None, min_data=None, drop_empty=Tr
         yield df_slice, metadata
 
 
-def calculate_target_values(
-    df,
-    target_dataframe_index,
-    labeling_function,
-    time_index,
-    window_size,
-    minimum_data=None,
-    maximum_data=None,
-    gap=None,
-    drop_empty=True,
-    verbose=False,
-    num_examples_per_instance=-1,
-):
-    assert labeling_function, "missing labeling function(s)"
-
-    #
-    if df.index.name != time_index:
-        df = df.set_index(time_index)
-
-    if isinstance(df[target_dataframe_index].dtype, pd.CategoricalDtype):
-        df[target_dataframe_index] = df[
-            target_dataframe_index
-        ].cat.remove_unused_categories()
-
-    records = []
-    label_name = _get_function_name(labeling_function)
-    for group_key, df_by_index in df.groupby(target_dataframe_index):
-        for dataslice, metadata in generate_data_slices(
-            df_by_index,
-            window_size=window_size,
-            gap=gap,
-            min_data=minimum_data,
-            drop_empty=drop_empty,
-        ):
-            record = labeling_function(dataslice)
-            records.append(
-                {
-                    target_dataframe_index: group_key,
-                    "cutoff_time": dataslice.first_valid_index(),
-                    label_name: record,
-                },
-            )
-    records = pd.DataFrame.from_records(records, index=None)
-    return records
-
-
 def _get_function_name(function):
     has_name = hasattr(function, "__name__")
     return function.__name__ if has_name else type(function).__name__
 
 
 def to_offset(value):
-    """Converts a value to an offset and validates the offset.
-
-    Args:
-        value (int or str or offset) : Value of offset.
-
-    Returns:
-        offset : Valid offset.
-    """
+    """Converts a pd.DateOffset object from string or pd.Timedelta object."""
+    error_msg = "offset must be greater than zero"
     if isinstance(value, int):
-        assert value > 0, "offset must be greater than zero"
+        assert value > 0, error_msg
         offset = value
-    elif isinstance(value, str):
-        error = "offset must be a valid string"
-        assert can_be_type(type=pd.tseries.frequencies.to_offset, string=value), error
+    elif isinstance(value, (str, pd.Timedelta)):
         offset = pd.tseries.frequencies.to_offset(value)
-        assert offset.n > 0, "offset must be greater than zero"
-    elif isinstance(value, pd.Timedelta):
-        offset = pd.tseries.frequencies.to_offset(value)
-        assert offset.n > 0, "offset must be greater than zero"
-    else:
-        assert issubclass(type(value), pd.tseries.offsets.BaseOffset), "invalid offset"
-        assert value.n > 0, "offset must be greater than zero"
+        assert offset.n > 0, error_msg
+    elif isinstance(value, DateOffset):
+        assert value.n > 0, error_msg
         offset = value
     return offset
 
 
-def can_be_type(type, string):
+def can_be_type(type_, string):
     try:
-        type(string)
+        type_(string)
         return True
-
     except ValueError:
         return False
 
 
 def cutoff_data(df, threshold):
-    """Cuts off data before the threshold.
+    """Cuts off data before the threshold. For example, if you have daily data points from
+        "2022-01-01" to "2022-01-05", and select a threshold of 2/2D/"2022-01-03", the data
+        will be cut off at "2022-01-03" (inclusive).
+
+        If the threshold is greater than the last time in the index, the data will be empty.
 
     Args:
         df (DataFrame) : Data frame to cutoff data.
@@ -197,24 +179,18 @@ def cutoff_data(df, threshold):
     if isinstance(threshold, int):
         assert threshold > 0, "threshold must be greater than zero"
         df = df.iloc[threshold:]
-
         if df.empty:
             return df, None
-
         cutoff_time = df.index[0]
-
     elif isinstance(threshold, str):
-        if can_be_type(type=pd.tseries.frequencies.to_offset, string=threshold):
+        if can_be_type(type_=pd.tseries.frequencies.to_offset, string=threshold):
             threshold = pd.tseries.frequencies.to_offset(threshold)
             assert threshold.n > 0, "threshold must be greater than zero"
             cutoff_time = df.index[0] + threshold
-
-        elif can_be_type(type=pd.Timestamp, string=threshold):
+        elif can_be_type(type_=pd.Timestamp, string=threshold):
             cutoff_time = pd.Timestamp(threshold)
-
         else:
             raise ValueError("invalid threshold")
-
     else:
         is_timestamp = isinstance(threshold, pd.Timestamp)
         assert is_timestamp, "invalid threshold"
@@ -222,8 +198,12 @@ def cutoff_data(df, threshold):
 
     if cutoff_time != df.index[0]:
         df = df[df.index >= cutoff_time]
-
         if df.empty:
             return df, None
-
     return df, cutoff_time
+
+
+def clean_date(date):
+    if isinstance(date, str):
+        return pd.Timestamp(datetime.strptime(date, "%Y-%m-%d"))
+    return date
